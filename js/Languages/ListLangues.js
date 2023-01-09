52469b50fa4dc252a7d3b9fc008d10d996b1baae
21harley <john.llanes.1996@gmail.com>
2023-01-08 22:34:54 -0400

    add form-contact

--- js/Languages/ListLangues.js
+++ js/Languages/ListLangues.js
@@ -9,7 +9,10 @@ const english={
         "Hi, nice to meet you :D, welcome to my Portfolio I hope you can get to know me better and see my projects.",
         "Close",
         "Download CV",
-        "I am a self-taught programmer, student of Computer Engineering of 7 semester besides being a full stack developer Vue-Laravel and React-Express. In my free time I teach programming classes in languages like c/c++ and java as well as designing user interfaces in figma."
+        "I am a self-taught programmer, student of Computer Engineering of 7 semester besides being a full stack developer Vue-Laravel and React-Express. In my free time I teach programming classes in languages like c/c++ and java as well as designing user interfaces in figma.",
+        "Name",
+        "Email",
+        "What did you think of the page?"
     ]
    }
 const spanish={
@@ -23,7 +26,10 @@ const spanish={
         "Hola muncho gusto :D, se bienvenido a mi Portafolio espero puedas, conocerme mejor y ver mis proyectos.",
         "Cerrar",
         "Descarga CV",
-        "Soy un programador autodidacta, estudiante de Ing. Informática de 7 semestre además de ser un developer full stack Vue-Laravel y React-Express. En mis tiempos libres doy clase de programación en lenguajes como c/c++ y java además de diseñar interfaces de usuario en figma."
+        "Soy un programador autodidacta, estudiante de Ing. Informática de 7 semestre además de ser un developer full stack Vue-Laravel y React-Express. En mis tiempos libres doy clase de programación en lenguajes como c/c++ y java además de diseñar interfaces de usuario en figma.",
+        "Nombre",
+        "Correo",
+        "¿Qué te pareció la página?"
     ]
 }
 
