26640c1d1b1270520ddff14a801ac3c7043d2784
21harley <john.llanes.1996@gmail.com>
2023-01-14 00:05:59 -0400

    add fix-cars-brifcase-and-font-icon

--- js/Languages/ListLangues.js
+++ js/Languages/ListLangues.js
@@ -12,7 +12,36 @@ const english={
         "I am a self-taught programmer, student of Computer Engineering of 7 semester besides being a full stack developer Vue-Laravel and React-Express. In my free time I teach programming classes in languages like c/c++ and java as well as designing user interfaces in figma.",
         "Name",
         "Email",
-        "What did you think of the page?"
+        "What did you think of the page?",
+        "",
+        "Description:",
+        "Repository",
+        "Page",
+        /*proyects*/
+        "Heads or Tails React Sass",
+        "Heads or tails game project using React, sass and BEM.",
+
+        "js calculator",
+        "Calculator creation project in js.",
+
+        "San Cristobal route system",
+        "San Cristóbal route management system with html, css, js, php and sql.",
+
+        "SPA test",
+        "Try SPA using responsive and css.",
+
+        "Express-pug project",
+        "Reminder notes project in html,css,js,pug,nodejs, express and mysql.",
+
+        "PostgreSQLJava Workshop",
+        "Program that manages a database with PostgreSQL and shows it with an interface made in java.",
+
+        "Course of computer pills php.",
+        "php classes, database connection, pagination, mail, login, personal pad and MVC.",
+
+        "Sokoban in c",
+        "Sokoban in c using structures, steps by references and files"
+
     ]
    }
 const spanish={
@@ -29,7 +58,35 @@ const spanish={
         "Soy un programador autodidacta, estudiante de Ing. Informática de 7 semestre además de ser un developer full stack Vue-Laravel y React-Express. En mis tiempos libres doy clase de programación en lenguajes como c/c++ y java además de diseñar interfaces de usuario en figma.",
         "Nombre",
         "Correo",
-        "¿Qué te pareció la página?"
+        "¿Qué te pareció la página?",
+        "",
+        "Descripción:",
+        "Repositorio",
+        "Pagina",
+         /*proyectos*/
+        "Cara o Cruz React Sass",
+        "Proyecto de juego cara o cruz usando React, sass y BEM.",
+
+        "Calculadora Js",
+        "Proyecto de creación de calculadora en js.",
+
+        "Sistema ruta San Cristóbal",
+        "Sistema de gestión de rutas de San Cristóbal con html,css,js,php y sql.",
+
+        "Prueba SPA.",
+        "Prueba SPA uso de responsiva y css.",
+
+        "Proyecto Express-pug",
+        "Proyecto de notas de recordatorio en html,css,js,pug,nodejs, express y mysql.",
+
+        "Taller PostgreSQL java",
+        "Programa que gestiona una base de datos con PostgreSQL y lo muestra con una interfaz hecha en java.",
+
+        "Curso de píldoras informáticas php.",
+        "Clases de php, conexión a base de datos, paginación, correo, login, bloc personal y MVC.",
+
+        "Sokoban en c",
+        "Sokoban en C utilizando estructuras, pasos por referencias y archivos."
     ]
 }
 
